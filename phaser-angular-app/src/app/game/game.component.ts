a78f430d9bd20c59e51c9ec7ed495bafbba95731
Jesús Alejandro Yahuitl Rodríguez <jesusaleyaro@hotmail.com>
2021-10-08 16:19:27 -0500

    game-container by id

--- phaser-angular-app/src/app/game/game.component.ts
+++ phaser-angular-app/src/app/game/game.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import Phaser from 'phaser';
 import { Scene } from './scene';
 
@@ -7,33 +7,33 @@ import { Scene } from './scene';
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css']
 })
-export class GameComponent implements OnInit {
+export class GameComponent implements OnInit, OnDestroy {
 
-  phaserGame: Phaser.Game;
+  phaserGame?: Phaser.Game;
   config: Phaser.Types.Core.GameConfig;
+  gameContainer: any;
 
 
-  constructor() { 
 
+
+  constructor() {
     this.config = {
-      type: Phaser.AUTO,
       width: 454,
       height: 313,
-      scene: [ Scene ],
-      parent: 'gameContainer',
+      scene: [Scene],
+      parent: 'game-container',
       pixelArt: true,
-      physics: {
-        default: 'arcade',
-        arcade: {
-          gravity: { y: 100 }
-        }
-      }
     };
-    this.phaserGame = new Phaser.Game(this.config);
   }
 
   ngOnInit(): void {
+    this.phaserGame = new Phaser.Game(this.config);
+  }
 
+  ngOnDestroy() {
+    if (this.phaserGame) {
+      this.phaserGame.destroy(true);
+    }
   }
 
 }
